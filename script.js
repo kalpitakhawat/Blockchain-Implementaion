0a7f422e231b647dca5890a0362ad1fe4beeb042
kalpitakhawat <kalpitakhawat@gmail.com>
2018-02-08 12:26:20 +0530

    static values assigned

--- script.js
+++ script.js
@@ -15,19 +15,24 @@ class Block {
 
 class Blockchain{
   constructor(){
-    this.chain = [this.getGenesisBlock()];
+    this.blockchain = [this.getGenesisBlock()];
   }
    getGenesisBlock() {
     return new Block(0,Date.now() , 'Genesis Block' , '0');
   }
    addBlock(block) {
-    block.prev_hash = this.chain[this.chain.length - 1].hash;
+    block.prev_hash = this.blockchain[this.blockchain.length - 1].hash;
     block.hash = block.calculateSelfHash();
-    this.chain.push(block);
+    this.blockchain.push(block);
   }
   displayChain(){
-    for (var i = 0; i < this.chain.length; i++) {
-      console.log(this.chain[i]);
+    for (var i = 0; i < this.blockchain.length; i++) {
+      console.log(this.blockchain[i]);
     }
   }
 }
+
+let bchain = new Blockchain();
+bchain.addBlock(new Block(1,Date.now(),'a-b:500'));
+bchain.addBlock(new Block(2,Date.now(),'b-c:200'));
+bchain.displayChain();
